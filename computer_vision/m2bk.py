9c4e3c1078ab7b036e83d7ab63bf18fde72df197
Allen Cutinha <allencutinha@gmail.com>
2023-01-30 12:20:43 +0100

    format code

--- computer_vision/m2bk.py
+++ computer_vision/m2bk.py
@@ -11,59 +11,61 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 class DatasetHandler:
-
     def __init__(self):
         # Define number of frames
         self.num_frames = 52
 
         # Set up paths
         root_dir_path = os.path.dirname(os.path.realpath(__file__))
-        self.image_dir = os.path.join(root_dir_path, '../data/week2-data/rgb')
-        self.depth_dir = os.path.join(root_dir_path, '../data/week2-data/depth')
+        self.image_dir = os.path.join(root_dir_path, "../data/week2-data/rgb")
+        self.depth_dir = os.path.join(root_dir_path, "../data/week2-data/depth")
 
         # Set up data holders
         self.images = []
         self.images_rgb = []
         self.depth_maps = []
 
-        self.k = np.array([[640, 0, 640],
-                           [0, 480, 480],
-                           [0,   0,   1]], dtype=np.float32)
-        
+        self.k = np.array([[640, 0, 640], [0, 480, 480], [0, 0, 1]], dtype=np.float32)
 
         # Read first frame
         self.read_frame()
-        print("\r" + ' '*20 + "\r", end='')
-        
+        print("\r" + " " * 20 + "\r", end="")
+
     def read_frame(self):
         self._read_depth()
         self._read_image()
-              
+
     def _read_image(self):
         for i in range(1, self.num_frames + 1):
             zeroes = "0" * (5 - len(str(i)))
             im_name = "{0}/frame_{1}{2}.png".format(self.image_dir, zeroes, str(i))
             self.images.append(cv.imread(im_name, flags=0))
             self.images_rgb.append(cv.imread(im_name)[:, :, ::-1])
-            print ("Data loading: {0}%".format(int((i + self.num_frames) / (self.num_frames * 2 - 1) * 100)), end="\r")
-            
-       
+            print(
+                "Data loading: {0}%".format(
+                    int((i + self.num_frames) / (self.num_frames * 2 - 1) * 100)
+                ),
+                end="\r",
+            )
+
     def _read_depth(self):
         for i in range(1, self.num_frames + 1):
             zeroes = "0" * (5 - len(str(i)))
             depth_name = "{0}/frame_{1}{2}.dat".format(self.depth_dir, zeroes, str(i))
-            depth = np.loadtxt(
-                depth_name,
-                delimiter=',',
-                dtype=np.float64) * 1000.0
+            depth = np.loadtxt(depth_name, delimiter=",", dtype=np.float64) * 1000.0
             self.depth_maps.append(depth)
-            print ("Data loading: {0}%".format(int(i / (self.num_frames * 2 - 1) * 100)), end="\r")
-            
-        
-def visualize_camera_movement(image1, image1_points, image2, image2_points, is_show_img_after_move=False):
+            print(
+                "Data loading: {0}%".format(int(i / (self.num_frames * 2 - 1) * 100)),
+                end="\r",
+            )
+
+
+def visualize_camera_movement(
+    image1, image1_points, image2, image2_points, is_show_img_after_move=False
+):
     image1 = image1.copy()
     image2 = image2.copy()
-    
+
     for i in range(0, len(image1_points)):
         # Coordinates of a point on t frame
         p1 = (int(image1_points[i][0]), int(image1_points[i][1]))
@@ -76,8 +78,8 @@ def visualize_camera_movement(image1, image1_points, image2, image2_points, is_s
 
         if is_show_img_after_move:
             cv.circle(image2, p2, 5, (255, 0, 0), 1)
-    
-    if is_show_img_after_move: 
+
+    if is_show_img_after_move:
         return image2
     else:
         return image1
@@ -100,7 +102,7 @@ def visualize_trajectory(trajectory):
 
     for i in range(0, trajectory.shape[1]):
         current_pos = trajectory[:, i]
-        
+
         locX.append(current_pos.item(0))
         locY.append(current_pos.item(1))
         locZ.append(current_pos.item(2))
@@ -135,29 +137,46 @@ def visualize_trajectory(trajectory):
     ZY_plt = plt.subplot(gspec[0, 1:])
     YX_plt = plt.subplot(gspec[1:, 0])
     traj_main_plt = plt.subplot(gspec[1:, 1:])
-    D3_plt = plt.subplot(gspec[0, 0], projection='3d')
+    D3_plt = plt.subplot(gspec[0, 0], projection="3d")
 
     # Actual trajectory plotting ZX
     toffset = 1.06
     traj_main_plt.set_title("Autonomous vehicle trajectory (Z, X)", y=toffset)
     traj_main_plt.set_title("Trajectory (Z, X)", y=1)
-    traj_main_plt.plot(locZ, locX, ".-", label="Trajectory", zorder=1, linewidth=1, markersize=4)
+    traj_main_plt.plot(
+        locZ, locX, ".-", label="Trajectory", zorder=1, linewidth=1, markersize=4
+    )
     traj_main_plt.set_xlabel("Z")
     # traj_main_plt.axes.yaxis.set_ticklabels([])
     # Plot reference lines
-    traj_main_plt.plot([locZ[0], locZ[-1]], [locX[0], locX[-1]], "--", label="Auxiliary line", zorder=0, linewidth=1)
+    traj_main_plt.plot(
+        [locZ[0], locZ[-1]],
+        [locX[0], locX[-1]],
+        "--",
+        label="Auxiliary line",
+        zorder=0,
+        linewidth=1,
+    )
     # Plot camera initial location
     traj_main_plt.scatter([0], [0], s=8, c="red", label="Start location", zorder=2)
     traj_main_plt.set_xlim([min, max])
     traj_main_plt.set_ylim([min, max])
-    traj_main_plt.legend(loc=1, title="Legend", borderaxespad=0., fontsize="medium", frameon=True)
+    traj_main_plt.legend(
+        loc=1, title="Legend", borderaxespad=0.0, fontsize="medium", frameon=True
+    )
 
     # Plot ZY
     # ZY_plt.set_title("Z Y", y=toffset)
     ZY_plt.set_ylabel("Y", labelpad=-4)
     ZY_plt.axes.xaxis.set_ticklabels([])
     ZY_plt.plot(locZ, locY, ".-", linewidth=1, markersize=4, zorder=0)
-    ZY_plt.plot([locZ[0], locZ[-1]], [(locY[0] + locY[-1]) / 2, (locY[0] + locY[-1]) / 2], "--", linewidth=1, zorder=1)
+    ZY_plt.plot(
+        [locZ[0], locZ[-1]],
+        [(locY[0] + locY[-1]) / 2, (locY[0] + locY[-1]) / 2],
+        "--",
+        linewidth=1,
+        zorder=1,
+    )
     ZY_plt.scatter([0], [0], s=8, c="red", label="Start location", zorder=2)
     ZY_plt.set_xlim([min, max])
     ZY_plt.set_ylim([minY, maxY])
@@ -167,7 +186,13 @@ def visualize_trajectory(trajectory):
     YX_plt.set_ylabel("X")
     YX_plt.set_xlabel("Y")
     YX_plt.plot(locY, locX, ".-", linewidth=1, markersize=4, zorder=0)
-    YX_plt.plot([(locY[0] + locY[-1]) / 2, (locY[0] + locY[-1]) / 2], [locX[0], locX[-1]], "--", linewidth=1, zorder=1)
+    YX_plt.plot(
+        [(locY[0] + locY[-1]) / 2, (locY[0] + locY[-1]) / 2],
+        [locX[0], locX[-1]],
+        "--",
+        linewidth=1,
+        zorder=1,
+    )
     YX_plt.scatter([0], [0], s=8, c="red", label="Start location", zorder=2)
     YX_plt.set_xlim([minY, maxY])
     YX_plt.set_ylim([min, max])
@@ -179,11 +204,11 @@ def visualize_trajectory(trajectory):
     D3_plt.set_xlim3d(min, max)
     D3_plt.set_ylim3d(min, max)
     D3_plt.set_zlim3d(min, max)
-    D3_plt.tick_params(direction='out', pad=-2)
+    D3_plt.tick_params(direction="out", pad=-2)
     D3_plt.set_xlabel("X", labelpad=0)
     D3_plt.set_ylabel("Z", labelpad=0)
     D3_plt.set_zlabel("Y", labelpad=-2)
-    
+
     # plt.axis('equal')
     D3_plt.view_init(45, azim=30)
     plt.tight_layout()
